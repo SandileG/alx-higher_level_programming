0e8247e301b73aacaa635dc7cdcff70077eea390
SandileG <sandilegraham@gmail.com>
2023-11-30 03:36:48 -0800

    5-print_string.py

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Underachiever Trying"
+# Print 3 the value of str followed by a new line.
+print(str * 3)
+# Print the first 9 characters of str followed by a new line.
+print(str[:9])
