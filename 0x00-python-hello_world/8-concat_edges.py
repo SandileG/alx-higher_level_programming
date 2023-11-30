9bfca1328bbe1de101c7e60b036b7c62307395c4
SandileG <sandilegraham@gmail.com>
2023-11-30 04:48:51 -0800

    8-concat_edges.py

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-language that combines remarkable power with very clear syntax"
-# Print the desired sentence using slicing.
-print(str[39:67] + str[107:112] + str[0:6])
+ language that combines remarkable power with very clear syntax"
+# Print the desired sentence using corrected slicing.
+print(str[39:67] + str[107:112] + str[:6])
