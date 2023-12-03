451d2fbc4f9d13402c48ead789e3844ab2d935d9
SandileG <sandilegraham@gmail.com>
2023-12-03 15:37:24 -0800

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,13 +3,14 @@ import random
 
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10
+try:
+    last_digit = number % 10
+except TypeError:
+    print("Last digit of", number, "is not an unteger")
 
-print("Last digit of", number, "is", last_digit, end=" ")
-
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
-else:
-    print("and is less than 6 and not 0")
+    if last_digit > 5:
+        print("and is greater than 5")
+    elif last_digit == 0:
+        print("and is 0")
+    else:
+        print("and is less than 6 and not 0")
