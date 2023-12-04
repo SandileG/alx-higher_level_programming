afd4b6985c98181f4e606bfa1976d75f12e6eab0
SandileG <sandilegraham@gmail.com>
2023-12-04 12:26:13 -0800

    10-add.py

--- 0x01-python-if_else_loops_functions/10-add.py
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 
-
 def add(a, b):
-    return (a + b)
+    try:
+        result = int(a) + int(b)
+        return result
+    except ValueError:
+        return None
 
-
-# Example usage:
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
+if __name__ == "__main__":
+    pass
