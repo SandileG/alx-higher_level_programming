eba078a4ab71f60ebbd0d06577622c2e650cb918
SandileG <sandilegraham@gmail.com>
2023-12-04 03:04:02 -0800

    8-uppercase.py

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def uppercase(s):
+    for char in s:
+        ascii_value = ord(char)
+        if 97 <= ascii_value <= 122:
+            print("{:c}".format(ascii_value - 32), end="")
+        else:
+            print("{:c}".format(ascii_value), end="")
+    print()
+
+
+uppercase("best")
+uppercase("Best School 98 Battery street")
