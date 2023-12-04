42eecb2c01b4a6e6fc8b23e67a40beb1cda511b2
SandileG <sandilegraham@gmail.com>
2023-12-04 02:29:03 -0800

    6-print_comb3.py

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+for tens_digit in range(10):
+    for units_digit in range(thens_digit + 1, 10):
+        print("{:d}{:d}".format(
+            tens_digit, units_digit
+        ), end=(", " if (tens_digit, units_digits) != (8, 9)else "\n"))
