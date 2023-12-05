62c1424981ff3501c1f4c90b895ce1e8ef5bb037
SandileG <sandilegraham@gmail.com>
2023-12-05 11:11:48 -0800

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
 
+
 def pow(a, b):
     result = a ** b
 
-    for _ in range(abs(b) - 1):
-        result *= a
     if b < 0:
         result = 1 / result
-
 
     return result
 
 
 # Test cases
 print(pow(2, 2))
+print(pow(-2, 2))
 print(pow(10, -2))
 print(pow(-98, -10))
