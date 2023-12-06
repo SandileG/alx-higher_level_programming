1638178784dfb6aaf8bb1535e38053d47077ae19
SandileG <sandilegraham@gmail.com>
2023-12-06 01:29:13 -0800

    2-replace_in_list.py

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def replace-in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(ly_list):
+        return (my_list)
+    my_list[idx] = element
+    return (my_list)
