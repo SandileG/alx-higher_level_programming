bd4ad8de3752efc8b05d22791a87b0be1afd54b6
SandileG <sandilegraham@gmail.com>
2023-12-06 10:35:13 -0800

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -7,45 +7,45 @@
  */
 int is_palindrome(listint_t **head)
 {
-    listint_t *slow, *fast, *prev, *temp;
-    int count, i;
+	listint_t *slow, *fast, *prev, *temp;
+	int count, i;
 
-    if (head == NULL || *head == NULL || (*head)->next == NULL)
-        return (1);
+	if (head == NULL || *head == NULL || (*head)->next == NULL)
+		return (1);
 
-    slow = *head;
-    fast = *head;
-    count = 0;
+	slow = *head;
+	fast = *head;
+	count = 0;
 
-    while (fast != NULL && fast->next != NULL)
-    {
-        slow = slow->next;
-        fast = fast->next->next;
-        count++;
-    }
+	while (fast != NULL && fast->next != NULL)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		count++;
+	}
 
-    prev = NULL;
-    temp = slow;
+	prev = NULL;
+	temp = slow;
 
-    while (temp != NULL)
-    {
-        temp = temp->next;
-        slow->next = prev;
-        prev = slow;
-        slow = temp;
-    }
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		slow->next = prev;
+		prev = slow;
+		slow = temp;
+	}
 
-    temp = prev;
-    slow = *head;
+	temp = prev;
+	slow = *head;
 
-    for (i = 0; i < count; i++)
-    {
-        if (slow->n != temp->n)
-            return (0);
+	for (i = 0; i < count; i++)
+	{
+		if (slow->n != temp->n)
+			return (0);
 
-        slow = slow->next;
-        temp = temp->next;
-    }
+		slow = slow->next;
+		temp = temp->next;
+	}
 
-    return (1);
+	return (1);
 }
