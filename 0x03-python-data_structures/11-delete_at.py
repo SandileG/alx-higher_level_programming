5fabc4e15292c0bc7a86556efa00b68fc0e573cd
SandileG <122836823+SandileG@users.noreply.github.com>
2023-12-06 19:33:46 +0200

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,6 +2,7 @@
 
 
 def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        return (my_list[:idx] + my_list[idx+1:])
-    return (my_list)
+    new_list = my_list.copy()
+    if 0 <= idx < len(new_list):
+        del new_list[idx]
+    return (new_list)
