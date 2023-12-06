5a530bc7ca947a3d5dd4554ecb4bdcf29f263b1c
SandileG <122836823+SandileG@users.noreply.github.com>
2023-12-06 14:17:10 +0200

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,6 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = (tuple_a[0] if len(tuple_a) >= 1 else 0, tuple_a[1] if len(tuple_a) >= 2 else 0)
-    b = (tuple_b[0] if len(tuple_b) >= 1 else 0, tuple_b[1] if len(tuple_b) >= 2 else 0)
-
-    result = (a[0] + b[0], a[1], b[1])
-
-    return (result)
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
