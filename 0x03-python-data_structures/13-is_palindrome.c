b5359f6b4b5a768bf452be3c28bdec368934a80f
SandileG <122836823+SandileG@users.noreply.github.com>
2023-12-06 20:30:01 +0200

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,31 +1,51 @@
 #include "lists.h"
 
 /**
- * is_palindrome - checks if a singly linked list is a palindrome.
- * @head: double pointer to the head of the linked list.
- * Return: 1 if palindrome, 0 otherwise.
+ * is_palindrome - checks if a linked list is a palindrome
+ * @head: double pointer to the head of the linked list
+ * Return: 1 if palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *current = *head;
-	int values[10000] ;
-	int count = 0;
+    listint_t *slow, *fast, *prev, *temp;
+    int count, i;
 
-	if (*head == NULL)
-		return (1);
+    if (head == NULL || *head == NULL || (*head)->next == NULL)
+        return (1);
 
-	while (current != NULL)
-	{
-		values[count] = current->n;
-		current = current->next;
-		count++;
-	}
+    slow = *head;
+    fast = *head;
+    count = 0;
 
-	for (int i = 0; i < count / 2; i++)
-	{
-		if (values[i] != values[count - i - 1])
-			return (0);
-	}
+    while (fast != NULL && fast->next != NULL)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        count++;
+    }
 
-	return (1);
+    prev = NULL;
+    temp = slow;
+
+    while (temp != NULL)
+    {
+        temp = temp->next;
+        slow->next = prev;
+        prev = slow;
+        slow = temp;
+    }
+
+    temp = prev;
+    slow = *head;
+
+    for (i = 0; i < count; i++)
+    {
+        if (slow->n != temp->n)
+            return (0);
+
+        slow = slow->next;
+        temp = temp->next;
+    }
+
+    return (1);
 }
