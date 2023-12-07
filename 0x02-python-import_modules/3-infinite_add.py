56c0b95dc4e8c447994796f19bf904dd4e3cec45
SandileG <sandilegraham@gmail.com>
2023-12-07 12:35:42 -0800

    3-infinite_add.py

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from sys import argv
+
+
+if __name__ == "__main__":
+    args = argv[1:]
+
+    # Ensure existence of arguments before attemptinh addition.
+    if args:
+        result = sum(map(int, args))
+	print(result)
+    else:
+        print(0)
