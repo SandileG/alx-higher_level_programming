241e828ddd7018ced5e139dc32a2068570c53d56
SandileG <122836823+SandileG@users.noreply.github.com>
2023-12-07 21:50:16 +0200

    Update 1-calculation.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     result_mul = mul(a, b)
     result_div = div(a, b)
 
-    print("{} + {} + {}".format(a, b, result_sub))
+    print("{} - {} + {}".format(a, b, result_sub))
     print("{} + {} + {}".format(a, b, result_add))
-    print("{} + {} + {}".format(a, b, result_div))
-    print("{} + {} + {}".format(a, b, result_mul))
+    print("{} / {} + {}".format(a, b, result_div))
+    print("{} * {} + {}".format(a, b, result_mul))
