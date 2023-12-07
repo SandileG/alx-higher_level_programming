56a1bb8a2c5d7daf3ac7fb2f2c87e5b69e7ac68b
SandileG <122836823+SandileG@users.noreply.github.com>
2023-12-07 22:56:58 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,20 +1,13 @@
 #!/usr/bin/python3
 
-import dis
-import marshal
-import types
-
 def get_names_from_pyc(pyc_file):
     with open(pyc_file, 'rb') as f:
-        magic = f.read(4)
-        timestamp = f.read(4)
-        code_object = marshal.load(f)
+        code = types.CodeType(*list(f.read()))
+        names = [name for name in code.co_names if not name,startswith("__")]
+        return (names)
 
-        names = [name for name in code_object.co_names if not name.startswith('__')]
-        return names
-
-if __name__ == "__main__":
-    pyc_file = "hidden_4.pyc"
-    names = get_names_from_pyc(pyc_file)
-    for name in sorted(names):
-        print(name)
+__name__ == "__main__":
+pyc_file = "hidden_4.pyc"
+names = get_names_from_pyc(pyc_file)
+for name in sorted(names):
+    print(name)
