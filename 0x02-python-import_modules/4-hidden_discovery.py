a240f74d231940a3f926d9ff4c5030c35dea8f07
SandileG <122836823+SandileG@users.noreply.github.com>
2023-12-07 22:59:17 +0200

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
-def get_names_from_pyc(pyc_file):
-    with open(pyc_file, 'rb') as f:
-        code = types.CodeType(*list(f.read()))
-        names = [name for name in code.co_names if not name,startswith("__")]
-        return (names)
+if __name__ == "__main__":
+    import hidden_4
 
-__name__ == "__main__":
-pyc_file = "hidden_4.pyc"
-names = get_names_from_pyc(pyc_file)
-for name in sorted(names):
-    print(name)
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)
