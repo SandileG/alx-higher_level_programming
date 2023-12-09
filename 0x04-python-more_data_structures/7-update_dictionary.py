0cf5298f2c1b9a86fd5aafa20a128301e5f8eb5a
SandileG <sandilegraham@gmail.com>
2023-12-09 02:47:08 -0800

    7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -14,34 +14,4 @@ def update_dictionary(a_dictionary, key, value):
         dict: The updated dictionary.
     """
     a_dictionary[key] = value
-    return a_dictionary
-
-# Test the function with the provided example
-a_dictionary = {'language': "C", 'number': 89, 'track': "Low level"}
-new_dict = update_dictionary(a_dictionary, 'language', "Python")
-
-# Print the updated dictionary
-for key, value in new_dict.items():
-    print("{}: {}".format(key, value))
-
-print("--")
-
-# Print the original dictionary
-for key, value in a_dictionary.items():
-    print("{}: {}".format(key, value))
-
-print("--")
-print("--")
-
-# Test with a new key
-new_dict = update_dictionary(a_dictionary, 'city', "San Francisco")
-
-# Print the updated dictionary
-for key, value in new_dict.items():
-    print("{}: {}".format(key, value))
-
-print("--")
-
-# Print the original dictionary
-for key, value in a_dictionary.items():
-    print("{}: {}".format(key, value))
+    return (a_dictionary)
