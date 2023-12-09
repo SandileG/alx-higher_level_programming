53e62b254f4b6fcc7f35b0896a40388851a42bdd
SandileG <sandilegraham@gmail.com>
2023-12-09 02:37:03 -0800

    6-print_sorted_dictionary.py

---
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+
+def print_sorted_dictionary(a_dictionary):
+    """
+    Prints a dictionary by ordered keys.
+
+    Args:
+        a_dictionary (dict): The input dictionary.
+
+    Returns:
+        None.
+    """
+    sorted_keys = sorted(a_dictionary.keys())
+
+    for key in sorted_keys:
+        print("{}: {}".format(key, a_dictionary[key]))
