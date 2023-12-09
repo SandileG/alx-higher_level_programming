84bd35d575dee5b6c42ffba573c83a101f4ac0f2
SandileG <122836823+SandileG@users.noreply.github.com>
2023-12-09 22:55:53 +0200

    Update 11-multiply_list_map.py

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -2,5 +2,4 @@
 
 
 def multiply_list_map(my_list=[], number=0):
-    return list(map(lambda x: x * number, my_list))
-    if my_list else []
+    return list(map(lambda x: x * number, my_list)) if my_list else []
