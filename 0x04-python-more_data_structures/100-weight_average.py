85ee93513730bb2ef75ade7f750202cd6cb99190
SandileG <sandilegraham@gmail.com>
2023-12-11 01:32:33 -0800

    100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -6,7 +6,7 @@ def weight_average(my_list=[]):
         return (0)
 
     total_score = 0
-    total_wright = 0
+    total_weight = 0
 
     for score, weight in my_list:
         total_score += score * weight
