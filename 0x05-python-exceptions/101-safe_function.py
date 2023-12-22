66581f064d105b58b27bbf3675e246af3c877059
SandileG <sandilegraham@gmail.com>
2023-12-22 12:25:11 -0800

    101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import sys
+
+
+def sage_function(fct, *args):
+    """
+    Executes a function safely/
+
+    :param fct: The function to be executed.
+    :param args: Arguments to be passed to the function.
+    :return: Result of the function or None if an exception occurs.
+    """
+
+    try:
+        result = fct(*args)
+        return (result)
+    except Exception as e:
+        print("Exception: {}".format(str(e)), file=sys.stderr)
+        return (None)
