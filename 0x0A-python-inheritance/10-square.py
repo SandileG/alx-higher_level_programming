2b7b4a0baec845e73d8e3159d5ce8428b69be0ef
SandileG <122836823+SandileG@users.noreply.github.com>
2024-01-15 10:55:03 +0200

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,45 +1,25 @@
 #!/usr/bin/python3
-"""
-Module containing the Square class.
-"""
+"""Module containing the Square class."""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-  """Class representing a square."""
+    """Class representing a square."""
 
-  def __init__(self, size=0):
-    """
-    Initializes a Square instance.
+    def __init__(self, size):
+        """
+        Initializes a Square instance.
 
-    Args:
-      size (int): The size of the square (default is 0).
-    """
+        Args:
+            size (int): The size of the square.
+        """
+        super().__init__(size, size)
 
-    super().__init__(size, size)
-    self.integer_validation("size", size)
+    def __str__(self):
+        """
+        Returns a string representation of the square.
 
-  def __str__(self):
-    """
-    Returns:
-      str: The string representation of the square.
-    """
-    return ("[Square] "
-        f"{self._Rectangle__width}/{self._Rectangle__height}")
-
-  def integer_validation(self, name, value):
-    """
-    Validates the input value.
-
-    Args:
-      name (str): The name of the value.
-      value (int): The value to be validated.
-
-    Raises:
-      TypeError: If the value is not an integer.
-      ValueError: If the value is less than or equal to 0.
-    """
-    if not isinstance(value, int):
-      raise TypeError("{} must be an integer".format(name))
-    if value <= 0:
-      raise ValueError("{} must be greater than 0".format(name))
+        Returns:
+            str: The string representation of the square.
+        """
+        return "[Square] {}/{}".format(self._Rectangle__width, self._Rectangle__height)
