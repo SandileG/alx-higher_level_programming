fcaf064ac4ac523b9dc55e706dcaae7e93b8c530
SandileG <sandilegraham@gmail.com>
2024-01-15 14:17:42 -0800

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,14 +6,12 @@ Defines a function that reads a text file (utf-8) and prints it to stdout.
 
 def read_file(filename=""):
     """
-    Reads a text file (UTF8) and prints it to stdout.
+    Reads the contents of a UTF-8 text file and prints it to stdout.
 
     Args:
-        filename (str): The name of the file to be read.
-
-    Returns:
-        None
+        filename (str): The name of the file to read.
     """
-    with open(filename, encoding="utf-8") as file:
-        content = file.read()
-        print(content)
+
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            print(line, end='') # print without extra newline.
