d59e4b94765d7a4589699524bc2156124ae16afd
SandileG <sandilegraham@gmail.com>
2024-02-18 13:23:20 -0800

    second_table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,3 +1,17 @@
--- INSERT MULTIPLE ROWS INTO second_table 
+-- Creates and fills a table second_table with attributes id, name, and score in my MySQL server with multiple rows.
 
-INSERT INTO second_tale (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+-- Check if the table exists
+CREATE TABLE IF NOT EXISTS `second_table` (
+    `id` INT,
+    `name` VARCHAR(256),
+    `score` INT
+);
+
+-- Add new records
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
+
+-- Display all records
+SELECT * FROM `second_table`;
