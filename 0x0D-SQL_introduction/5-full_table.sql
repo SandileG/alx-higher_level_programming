e0436db0fecc9823b4fbca161cdf923135d15d9a
SandileG <sandilegraham@gmail.com>
2024-02-18 05:48:55 -0800

    print

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,11 +1,2 @@
--- Print full description of the table first_table
-SELECT CONCAT(
-    'first_table',
-    'CREATE TABLE `first_table` (',
-    '`id` int NOT NULL AUTO_INCREMENT,',
-    '`name` varchar(128) DEFAULT NULL,',
-    '`c` char(1) DEFAULT NULL,',
-    '`created_at` date DEFAULT NULL,',
-    'PRIMARY KEY (`id`)',
-    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
-) AS Description;
+-- Prints the full description of the table first_table in my MySQL server.
+SHOW CREATE TABLE `first_table`;
