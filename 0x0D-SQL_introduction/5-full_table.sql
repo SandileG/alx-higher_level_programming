1c9539cb9e7bf673b70d2beb55434e2fd13f9b65
SandileG <sandilegraham@gmail.com>
2024-02-18 05:47:31 -0800

    print

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,7 +1,11 @@
--- PRINT FULL DESCRIPTON OF THE TABLE first_table
-
-SELECT CONCAT('Table\tCreate Table') AS Description FROM DUAL
-UNION ALL
-SELECT table_name, create_statement
-FROM information_schema.tables
-WHERE table_schema = 'hbtn_0c_0' AND table_name = 'first_table';
+-- Print full description of the table first_table
+SELECT CONCAT(
+    'first_table',
+    'CREATE TABLE `first_table` (',
+    '`id` int NOT NULL AUTO_INCREMENT,',
+    '`name` varchar(128) DEFAULT NULL,',
+    '`c` char(1) DEFAULT NULL,',
+    '`created_at` date DEFAULT NULL,',
+    'PRIMARY KEY (`id`)',
+    ') ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;'
+) AS Description;
