684dd64031f8a0d7730fb4891f53e1125366e08a
SandileG <sandilegraham@gmail.com>
2024-02-19 04:54:53 -0800

    0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,20 +1,5 @@
 -- Script to list privileges of MySQL users user_0d_1 and user_0d_2 on localhost
 
--- Create user user_0d_1 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
-
--- Grant all privileges to user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
-
--- Create user user_0d_2 if not exists
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost';
-
--- Grant all privileges to user_0d_2
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost';
-
--- Flush privileges to apply changes
-FLUSH PRIVILEGES;
-
 -- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
