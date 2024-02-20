4e7fa0476dc20ca837a55a3e69803082c0ce935f
SandileG <sandilegraham@gmail.com>
2024-02-20 01:24:14 -0800

    create

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,15 @@
+-- SCRIPT TO CREATE DATABASE hbtn_0d_usa AND TABLE CITIES
+
+-- Create database hbtn_0d_usa if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- Use the htbn_0d_usa database
+USE htbn_0d_usa;
+
+-- Create table cities if exists
+CREATE TABLE IF NOT EXISTS cities (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+);
