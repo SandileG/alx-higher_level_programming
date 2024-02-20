3681443aae5c13b3d9700580cbabbc40831e8269
SandileG <sandilegraham@gmail.com>
2024-02-20 01:27:31 -0800

    create

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,15 +1,22 @@
--- SCRIPT TO CREATE DATABASE hbtn_0d_usa AND TABLE CITIES
+-- Script to create database hbtn_0d_usa and table cities
 
 -- Create database hbtn_0d_usa if not exists
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
--- Use the htbn_0d_usa database
-USE htbn_0d_usa;
+-- Use the hbtn_0d_usa database
+USE hbtn_0d_usa;
 
--- Create table cities if exists
+-- Create table cities if not exists
 CREATE TABLE IF NOT EXISTS cities (
-	id INT AUTO_INCREMENT PRIMARY KEY,
-	state_id INT NOT NULL,
-	name VARCHAR(256) NOT NULL,
-	FOREIGN KEY (state_id) REFERENCES states(id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
+
+-- Insert data into cities table
+INSERT INTO cities (state_id, name) VALUES
+(1, 'San Francisco'),
+(1, 'San Diego'),
+(2, 'Page'),
+(2, 'Phoenix');
