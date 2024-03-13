cefa759cac4ac2d37aadd467269958e11b3dfef7
SandileG <sandilegraham@gmail.com>
2024-03-13 04:56:33 -0700

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 /* Create constant variable called myVar with value "JavaScript is amazing" */
-const myVar = "JavaScript is amazing";
+const myVar = 'JavaScript is amazing';
 
 /* Use console.log(...) to print the output */
 console.log(myVar);
