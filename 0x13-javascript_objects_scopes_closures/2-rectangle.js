5aae245cf4021fe44605559f9871bf9e15461d91
SandileG <122836823+SandileG@users.noreply.github.com>
2024-03-16 23:20:42 +0200

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,16 +2,11 @@
 
 class Rectangle {
   constructor (w, h) {
-    this.width = w;
-    this.height = h;
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
   }
 }
 
-function createRectangle (w, h) {
-  if (w <= 0 || h <= 0 || isNaN(w) || isNaN(h)) {
-    return {};
-  }
-  return new Rectangle(w, h);
-}
-
-module.exports = createRectangle;
+module.exports = Rectangle;
