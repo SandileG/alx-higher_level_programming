bd6e88396372645a385c2e6732fd3666341d954f
SandileG <122836823+SandileG@users.noreply.github.com>
2024-03-17 21:19:25 +0200

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,38 +1,16 @@
 #!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-def select_states(username, password, database):
-    # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=username, passwd=password, db=database)
-
-    # Create a cursor object
-    cur = db.cursor()
-
-    # Execute SQL query to select all states sorted by id
-    cur.execute("SELECT * FROM states ORDER BY id")
-
-    # Fetch all rows
-    rows = cur.fetchall()
-
-    # Display results
-    for row in rows:
-        print(row)
-
-    # Close cursor and database connection
-    cur.close()
-    db.close()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
-    
-    # Get MySQL credentials from command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    # Call function to select states
-    select_states(username, password, database)
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
