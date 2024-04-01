8a4ad162de3e1a3b3080bfbccf56c9915725d47e
SandileG <122836823+SandileG@users.noreply.github.com>
2024-04-01 22:21:39 +0200

    Update 5-hbtn_header.py

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Sends a request to the URL and displays the value of the variable X-Request-Id
-in the response header"""
+Launches request to URL and displays value of variable X-Request-Id
+in response header"""
 
 if __name__ == '__main__':
     from requests import get
     from sys import argv
 
-    res = get(argv[1])
-    print(res.headers.get('X-Request-Id'))
+    pet= get(argv[1])
+    print(pet.headers.get('X-Request-Id'))
