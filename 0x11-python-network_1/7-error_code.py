0b6a800b4a1353669e47029128e9b5c34c4a3676
SandileG <sandilegraham@gmail.com>
2024-04-01 13:42:07 -0700

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-import requests
-import sys
+"""Sends a request to the URL and displays the body of the response."""
+
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    
-    response = requests.get(url)
-    
-    if response.status_code >= 400:
-        print("Error code:", response.status_code)
-    else:
-        print(response.text)
+    from sys import argv
+    from requests import get
+
+    linkit = argv[1]
+
+    answer = get(linkit)
+    BAD_TXT = 'Error code: {}'
+    state = answer.status_code
+    print(BAD_TXT.format(state) if (state >= 400) else answer.text)
