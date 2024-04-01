4f3feb8309764e8acf94aa5d183dd7925f90e705
SandileG <122836823+SandileG@users.noreply.github.com>
2024-04-01 21:53:26 +0200

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-Sends a POST request to the passed URL with the email as a parameter,
-and displays the body of the response (decoded in utf-8)
+Launches POST request to passed URL with email as parameter,
+and displays body of response (decoded in utf-8)
 """
 
-if __name__ == '__main__':
     import urllib.request
     import urllib.parse
     import sys
