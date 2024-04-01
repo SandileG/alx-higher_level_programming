f5cdfd24c1e097ae1497bf992c49a23ca4872856
SandileG <sandilegraham@gmail.com>
2024-04-01 13:51:40 -0700

    8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-import requests
-import sys
+"""
+Launch request to URL and displays body of the response.
+"""
+
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        q = ""
-    else:
-        q = sys.argv[1]
-    
-    url = 'http://0.0.0.0:5000/search_user'
-    data = {'q': q}
-    
-    response = requests.post(url, data=data)
-    
-    try:
-        json_data = response.json()
-        
-        if json_data:
-            print("[{}] {}".format(json_data['id'], json_data['name']))
+    from requests import post
+    from sys import argv
+
+    Mega_link = 'http://0.0.0.0:5000/search_user'
+    info = {'q': argv[1] if len(argv) >= 2 else ""}
+    answer = post(Mega_link, info)
+
+    type_pet = answer.headers['content-type']
+
+    if type_pet == 'application/json':
+        result = answer.json()
+        _id = result.get('id')
+        name = result.get('name')
+        if (result != {} and _id and name):
+            print("[{}] {}".format(_id, name))
         else:
-            print("No result")
-    except ValueError:
-        print("Not a valid JSON")
+            print('No result')
+    else:
+        print('Not a valid JSON')
