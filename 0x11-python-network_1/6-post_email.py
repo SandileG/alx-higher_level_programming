eed9751b897b2c9d364e68c1614559370ccbca66
SandileG <122836823+SandileG@users.noreply.github.com>
2024-04-01 22:32:28 +0200

    Update 6-post_email.py

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-import requests
-import sys
+"""
+Sends a POST request to the passed URL with the email as a parameter,
+and finally displays the body of the response."""
+
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    email = sys.argv[2]
-    
-    response = requests.post(url, data={'email': email})
-    
-    print("Your email is:", email)
-    print(response.text)
+    from sys import argv
+    from requests import post
+
+    link = argv[1]
+    email = argv[2]
+    pet = post(link, {'email': email})
+    print(pet.text)
