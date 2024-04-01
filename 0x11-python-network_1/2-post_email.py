08f77a25c6a49aefd61907abe5d684da23973ee8
SandileG <sandilegraham@gmail.com>
2024-04-01 12:48:44 -0700

    2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-import urllib.request
-import urllib.parse
-import sys
 
-if len(sys.argv) < 3:
-    print("Usage: {} <URL> <email>".format(sys.argv[0]))
-    sys.exit(1)
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.parse
+    import sys
 
-url = sys.argv[1]
-email = sys.argv[2]
+    if len(sys.argv) < 3:
+        print("Usage: {} <URL> <email>".format(sys.argv[0]))
+        sys.exit(1)
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-req = urllib.request.Request(url, data=data, method='POST')
+    url = sys.argv[1]
+    email = sys.argv[2]
+    DATA = urllib.parse.urlencode({"email": email})
+    DATA = DATA.encode('ascii')
 
-with urllib.request.urlopen(req) as response:
-    body = response.read().decode('utf-8')
-    print(body
+    with urllib.request.urlopen(url, DATA) as response:
+        print(response.read().decode('utf-8'))
