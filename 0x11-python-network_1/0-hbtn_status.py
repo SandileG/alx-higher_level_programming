8c7d8eded9d109f2949e8f643a3a8ae90fcc4f83
SandileG <122836823+SandileG@users.noreply.github.com>
2024-04-01 21:01:47 +0200

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,16 @@
-#!/usr/bin/python3
 import urllib.request
+import sys
 
-url = 'https://alx-intranet.hbtn.io/status'
+# Get the URL from the command-line argument
+url = sys.argv[1]
 
+# Open the URL using a with statement for proper resource management
 with urllib.request.urlopen(url) as response:
-    body = response.read()
-    utf8_content = body.decode('utf-8')
+   # Extract the headers from the response
+   headers = response.info()
 
-print("Body response:")
-print("\t- type", type(body))
-print("\t- content:", body)
-print("\t- utf8 content:", utf8_content)
+   # Retrieve the X-Request-Id from the headers
+   request_id = headers.get("X-Request-Id")
+
+   # Print the retrieved X-Request-Id value
+   print(request_id)
