0e6ab4c70722d6e85a5460ed1b61531caf871eb6
SandileG <sandilegraham@gmail.com>
2024-04-02 03:03:37 -0700

    6-peak.

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,9 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unstored integers."""
+"""Algorithmic list of integers."""
 
 
 def find_peak(list_of_integers):
-    if not list_of_integers:
-        return None
-
-    left, right = 0, len(list_of_integers) - 1
-
-    while left < right:
-        mid = (left + right) // 2
-
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
-            right = mid
-
-    return list_of_integers[left]
+    """Finds a peak in a list of unsorted integers."""
+    if list_of_integers:
+        list_of_integers.sort(reverse=True)
+        return list_of_integers[0]
